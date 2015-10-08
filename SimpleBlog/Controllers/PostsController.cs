9a076e8ed22f07ca2edcabca48c05e32dabb5f83
Jorge Torreblanca <torrenegra.inv@gmail.com>
2015-10-08 12:13:47 -0500

    Added some basic views and layout

--- SimpleBlog/Controllers/PostsController.cs
+++ SimpleBlog/Controllers/PostsController.cs
@@ -6,7 +6,7 @@ namespace SimpleBlog.Controllers
     {
         public ActionResult Index()
         {
-            return Content("Hello, World!!!");
+            return View();
         }
     }
 }
